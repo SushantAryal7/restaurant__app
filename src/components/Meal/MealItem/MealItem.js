0591b27fd64b08f23596b927ba5d040958eee997
sushant Aryal <aryasushant7@gmail.com>
2024-07-06 15:22:46 +0530

    complete upto 8 module

--- src/components/Meal/MealItem/MealItem.js
+++ src/components/Meal/MealItem/MealItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 
 
@@ -7,9 +8,14 @@ function MealItem(props) {
     const price = `$${props.price.toFixed(2)}`;
   return (
     <li className={classes.meal}>
+        <div>
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
+        </div>
+        <div>
+            <MealItemForm />
+        </div>
     </li>
   )
 }
